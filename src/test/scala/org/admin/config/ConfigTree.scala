ec083882bc8218de23b608491f5ea71315688bbe
Gennadi Heimann <genaheimann@gmail.com>
2016-11-16 23:03:49 +0100

    specs for ConfigTree

---
+++ src/test/scala/org/admin/config/ConfigTree.scala
@@ -0,0 +1,26 @@
+package org.admin.config
+
+import org.specs2.Specification
+
+/**
+  * Created by gennadi on 16.11.16.
+  */
+class ConfigTree extends Specification{
+
+  def is = s2"""
+    Diese Specification erstellt die Konfiguration
+      autentification
+      1. step
+      2. step
+      3. step
+      4. step
+      5. step
+      6. step
+      7. step
+
+
+
+
+    """
+
+}
