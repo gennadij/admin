cb5acac951c0dd2bf9d7e2d117db2a1f62634c28
heimann <heimann@172.29.10.121>
2016-11-17 15:57:48 +0100

    settings for testing db

--- src/main/scala/org/persistence/db/orientdb/OrientDB.scala
+++ src/main/scala/org/persistence/db/orientdb/OrientDB.scala
@@ -7,17 +7,42 @@ import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal
 object OrientDB {
   
   
-  def setGraph(uri: String){
+  def setGraph(){
     
   }
   
   def getGraph(): OrientGraph = {
-    val uri = if(System.getProperty("os.name") == "Linux") 
-                "remote:localhost/config1" 
-              else 
-                "remote:localhost/config_1"
+    val xmlDBConfig = scala.xml.XML.loadFile("global_config/globalConfig.xml")
     
-    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, "root", "root")
+    val dbName = (xmlDBConfig \ "globalConfig" \ "db" \ "dbName").text
+    val username = (xmlDBConfig \ "globalConfig" \ "db" \ "username").text
+    val password = (xmlDBConfig \ "globalConfig" \ "db" \ "password").text
+    
+    val uri = if(System.getProperty("os.name") == "Linux") 
+                "remote:localhost/" + dbName 
+              else 
+                "remote:localhost/" + dbName
+    
+    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, username, password)
     factory.getTx()
+  }
+  
+  def getGraph(db: String, username: String, password: String) = {
+    val uri = if(System.getProperty("os.name") == "Linux") 
+                "remote:localhost/" + db 
+              else 
+                "remote:localhost/" + db
+    
+    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, username, password)
+    factory.getTx()
+  }
+  
+  
+  private def getDBConfig = {
+    val xmlDBConfig = scala.xml.XML.loadFile("global_config/globalConfig.xml")
+    
+    val dbName = (xmlDBConfig \ "globalConfig" \ "db" \ "dbName").text
+    val username = (xmlDBConfig \ "globalConfig" \ "db" \ "username").text
+    val password = (xmlDBConfig \ "globalConfig" \ "db" \ "password").text
   }
 }
--- src/test/scala/org/admin/config/ConfigTree.scala
+++ src/test/scala/org/admin/config/ConfigTree.scala
@@ -1,6 +1,8 @@
 package org.admin.config
 
 import org.specs2.Specification
+import play.api.libs.json.Json
+import org.persistence.db.orientdb.OrientDB
 
 /**
   * Created by gennadi on 16.11.16.
@@ -9,7 +11,7 @@ class ConfigTree extends Specification{
 
   def is = s2"""
     Diese Specification erstellt die Konfiguration
-      autentification
+      autentification                   
       1. step
       2. step
       3. step
@@ -17,10 +19,34 @@ class ConfigTree extends Specification{
       5. step
       6. step
       7. step
-
-
-
-
+      8. step
     """
 
+//  val db = "testDB"
+//  val username = "root"
+//  val password = "root"
+//  OrientDB.getGraph(db, username, password)
+  
+  
+  val autentificationClientServer = Json.obj(
+    "jsonId" -> 2,
+    "method" -> "autheticate"
+    ,"params" -> Json.obj(
+      "username" -> "test",
+      "password" -> "test"
+    )
+  )
+//  {"jsonId": 2, "method": "autheticate", result: {"id": "AU#40:0", "username": "test", "authentication": true}}
+//  val jsonServerClient3 = handelMessage(jsonClientServer3)
+  def e1 = (autentificationClientServer \ "jsonId").asOpt[Int] must_== Some(2)
+//  def e11 = (jsonServerClient3 \ "method").asOpt[String] must_== Some("autheticate")
+//  def e12 = (jsonServerClient3 \ "result" \ "adminId").asOpt[String].get must_==
+//    (jsonServerClient2 \ "result" \ "adminId").asOpt[String].get
+//  def e13 = (jsonServerClient3 \ "result" \ "username").asOpt[String].get must_==
+//    (jsonServerClient2 \ "result" \ "username").asOpt[String].get
+//  def e14 = (jsonServerClient3 \ "result" \ "authentication").asOpt[Boolean].get must_==
+//    (jsonServerClient2 \ "result" \ "authentication").asOpt[Boolean].get
+//}
+  
+  
 }
