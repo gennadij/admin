8590a3af31735c503c65ae410071010d9ceb0f85
Gennadi Heimann <genaheimann@gmail.com>
2017-02-08 00:21:00 +0100

    deactivate global config for db in xml

--- src/main/scala/org/persistence/db/orientdb/OrientDB.scala
+++ src/main/scala/org/persistence/db/orientdb/OrientDB.scala
@@ -4,6 +4,7 @@ import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory
 
 import com.tinkerpop.blueprints.impls.orient.OrientGraph
 import com.orientechnologies.orient.core.db.ODatabaseRecordThreadLocal
+import org.persistence.GlobalConfigForDB
 
 /**
  * Copyright (C) 2016 Gennadi Heimann genaheimann@gmail.com
@@ -24,21 +25,27 @@ object OrientDB {
    */
   def getGraph(): OrientGraph = {
     
-    val xmlDBConfig = scala.xml.XML.loadFile("global_config/globalConfig.xml")
-    
-    val activeDB: String = (xmlDBConfig \"activeDB" \ "dbName").text
-    val dbs : scala.xml.NodeSeq = (xmlDBConfig \ "db")
-    
-    val db: List[DB] = dbs.filter (k => (k \ "dbName").text == activeDB ).map(db =>{
-      new DB((db \ "kind").text, (db \ "dbName").text, (db \ "username").text, (db \ "password").text)
-    }).toList
+//    val xmlDBConfig = scala.xml.XML.loadFile("global_config/globalConfig.xml")
+//    
+//    val activeDB: String = (xmlDBConfig \"activeDB" \ "dbName").text
+//    val dbs : scala.xml.NodeSeq = (xmlDBConfig \ "db")
+//    
+//    val db: List[DB] = dbs.filter (k => (k \ "dbName").text == activeDB ).map(db =>{
+//      new DB((db \ "kind").text, (db \ "dbName").text, (db \ "username").text, (db \ "password").text)
+//    }).toList
     
     val uri = if(System.getProperty("os.name") == "Linux") 
-                "remote:localhost/" + db(0).dbName
+      "remote:localhost/" + GlobalConfigForDB.db.dbName
+//                "remote:localhost/" + db(0).dbName
               else 
-                "remote:localhost/" + db(0).dbName
+//                "remote:localhost/" + db(0).dbName
+      "remote:localhost/" + GlobalConfigForDB.db.dbName
     
-    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, db(0).username, db(0).password)
+                
+                
+                
+//    val factory:  OrientGraphFactory = new OrientGraphFactory(uri, db(0).username, db(0).password)
+      val factory:  OrientGraphFactory = new OrientGraphFactory(uri, GlobalConfigForDB.db.username, GlobalConfigForDB.db.password)
     factory.getTx()
   }
 }
