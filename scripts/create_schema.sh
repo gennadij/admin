3fce69af5adfe842a0e05bf4d1e2579f091d6b3c
Gennadi Heimann <genaheimann@gmail.com>
2017-05-02 13:46:23 +0200

    Schema für ChangeLog v012 wurde erzeugt

--- scripts/create_schema.sh
+++ scripts/create_schema.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-cd /home/gennadi/development/projects/admin/docu/db_schmas/v011
+cd /home/gennadi/development/projects/admin/docu/db_schmas/$1
 
-/home/gennadi/development/tools/orientdb/bin/console.sh db_schema_v011_1.txt
+/home/gennadi/development/tools/orientdb/bin/console.sh db_schema.txt
