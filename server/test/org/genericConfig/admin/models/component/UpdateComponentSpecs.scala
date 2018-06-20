b83fd69343128488b4708ecdc09cdb96f6c5088a
Gennadi Heimann <gennadiheimann@gmail.com>
2018-06-20 23:28:18 +0200

    server -> update Components specs ok

--- server/test/org/genericConfig/admin/models/component/UpdateComponentSpecs.scala
+++ server/test/org/genericConfig/admin/models/component/UpdateComponentSpecs.scala
@@ -4,7 +4,7 @@ import org.genericConfig.admin.controllers.websocket.WebClient
 import org.genericConfig.admin.shared.common.json.JsonNames
 import org.genericConfig.admin.shared.common.status.Success
 import org.genericConfig.admin.shared.component.json.{JsonComponentIn, JsonComponentParams}
-import org.genericConfig.admin.shared.component.status.{AddComponentSuccess, AppendComponentSuccess}
+import org.genericConfig.admin.shared.component.status.{AddComponentSuccess, AppendComponentSuccess, UpdateComponentSuccess}
 import org.genericConfig.admin.shared.config.status.StatusAddConfig
 import org.genericConfig.admin.shared.user.status.{AddUserAlreadyExist, AddUserError, AddUserSuccess}
 import org.specs2.mutable.Specification
@@ -42,8 +42,8 @@ class UpdateComponentSpecs extends Specification
         val configTreeBO = getConfigTree(configId)
 
         this.stepId = configTreeBO.configTree.get.stepId
-        this.componentId = addComponentToStep(this.stepId, "Component", "immutable")._1
 
+        this.componentId = addComponentToStep(this.stepId, "Component", "immutable")._1
       case s if AddUserError().status == s =>
         Logger.info("Fehler bei der Vorbereitung")
     }
@@ -74,13 +74,11 @@ class UpdateComponentSpecs extends Specification
       Logger.info("OUT " + jsonAddComponentOut)
 
       (jsonAddComponentOut \ "json").asOpt[String] === Some(JsonNames.UPDATE_COMPONENT)
-      (jsonAddComponentOut \ "result" \ "componentId").asOpt[String].get.size === 10
+      (jsonAddComponentOut \ "result" \ "componentId").asOpt[String].get.size === 32
       (jsonAddComponentOut \ "result" \ "nameToShow").asOpt[String] === Some("Component 1 updated")
       (jsonAddComponentOut \ "result" \ "kind").asOpt[String] === Some("immutable updated")
-      (jsonAddComponentOut \ "result" \ "status" \ "addComponent" \ "status").asOpt[String] ===
-        Some(AddComponentSuccess().status)
-      (jsonAddComponentOut \ "result" \ "status" \ "appendComponent" \ "status").asOpt[String] ===
-        Some(AppendComponentSuccess().status)
+      (jsonAddComponentOut \ "result" \ "status" \ "updateComponent" \ "status").asOpt[String] ===
+        Some(UpdateComponentSuccess().status)
       (jsonAddComponentOut \ "result" \ "status" \ "common" \ "status").asOpt[String] === Some(Success().status)
 
 
